9ba0fd4c4c593dd2351f5acad97984481c764ccd
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2021-04-08 17:56:55 +0900

    Add files via upload

---
+++ mergeKlist.py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import heapq
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+class Solution:
+    def mergeKLists(self, lists: [ListNode]) -> ListNode:
+        root = result = ListNode(None)
+        heap = []
+
+        #각 연결 리스트의 루트를 힙에 저장
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        #힙 추출 이후 다음 노드는 다시 저장
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
